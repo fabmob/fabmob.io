a0c9ad407501f44212d3f986ec2071df96dd3be5
Mael <mael.thomas@data.gouv.fr>
2020-04-15 18:01:14 +0200

    TEC passage à MVP.Css

--- Accueil.js
+++ Accueil.js
@@ -2,103 +2,88 @@ import React from "react";
 
 export default () => (
   <div>
-    <div
-      css={`
-        height: 100vh;
-        box-shadow: 0 12px 12px -10px rgba(48, 41, 41, 0.1);
-        display: flex;
-        flex-wrap: wrap;
-        align-items: center;
-      `}
-    >
-      <div
-        css={`
-          width: 80%;
-          margin: 0 auto;
-          h1 {
-            font-size: 350%;
-            font-weight: 600;
-            margin: 1.2rem 0;
-          }
-          p {
-            font-size: 150%;
-          }
-
-          @media (max-width: 800px) {
-          }
-        `}
-      >
-        <header>
-          <img
-            css="width: 20rem"
-            src="http://lafabriquedesmobilites.fr/wp-content/themes/fabmob/images/fabmob-logo.svg"
-          ></img>
-          <p>Révolutionner la mobilité autour des communs !</p>
-        </header>
-      </div>
-
-      <div
-        href="#quoi"
-        css={`
-          width: 100%;
-          a {
-            display: block;
-            background: white;
-            border-radius: 0.3rem;
-            width: 7rem;
-            text-align: center;
-            cursor: pointer;
-            text-decoration: none;
-            padding: 0.2rem 0.6rem;
-            font-size: 130%;
-            color: black;
-            text-transform: uppercase;
-            font-weight: 600;
-            margin: 1rem auto;
-          }
-        `}
-      >
-        <a href="#quoi">Découvrir</a>
-      </div>
+    <div css={``}>
+      <header>
+        <nav>
+          <a href="https://andybrewer.github.io/mvp/">
+            <img
+              css="width: 10rem"
+              src="http://lafabriquedesmobilites.fr/wp-content/themes/fabmob/images/fabmob-logo.svg"
+            ></img>
+          </a>
+          <ul>
+            <li>
+              <a href="#docs">A propos</a>
+            </li>
+            <li>
+              <a href="#faq">FAQ</a>
+            </li>
+            <li>
+              <a href="https://www.github.com/andybrewer/mvp/" target="_blank">
+                Wiki ↗
+              </a>
+            </li>
+          </ul>
+        </nav>
+        <h1>
+          Révolutionner la mobilité via les <u>communs</u>
+        </h1>
+        <p>
+          Une association 1901 pour construire les nouvelles formes de mobilité
+          en réunissant tous les acteurs autour de{" "}
+          <mark>communs open source</mark>
+        </p>
+        <br />
+        <p>
+          <a download="mvp.html" href="./mvp.html" target="_blank">
+            <i>HTML Template ↗</i>
+          </a>
+          <a download="mvp.css" href="./mvp.css" target="_blank">
+            <b>MVP.css (8kb) ↗</b>
+          </a>
+        </p>
+      </header>
     </div>
     <Présentation />
   </div>
 );
 
 let Présentation = () => (
-  <div
-    id="quoi"
-    css={`
-      margin: 0 auto;
-      max-width: 40rem;
-      padding: 0.3rem 1rem;
-      ul {
-        list-style-type: none;
-        padding-left: 1rem;
-      }
-      em {
-        color: #6c5ce7;
-        font-style: normal;
-      }
-      h2 {
-        font-weight: 600;
-        font-size: 140%;
-        padding-right: 2rem;
-        margin: 3rem 0 1.2rem;
-        line-height: 1.4rem;
-      }
-      .illustration {
-        font-size: 3rem;
-        display: block;
-        margin: 0 auto;
-      }
-      @media (max-width: 800px) {
-        .illustration {
-          text-align: center;
-        }
-      }
-    `}
-  >
-    <h2>Blablabla</h2>
-  </div>
+  <main>
+    <section>
+      <header>
+        <h2>Un concept, des fabriques</h2>
+        <p>
+          Le modèle de la fabrique, initié en France, se répliquer dans d'autres
+          pays et domaines
+        </p>
+      </header>
+      <aside>
+        <img
+          css="width: 10rem"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Flag_of_France.svg/320px-Flag_of_France.svg.png?uselang=fr"
+        ></img>
+        <h3> FabMob France</h3>
+        <p>Blablabla </p>
+        <p>
+          <a href="#">
+            <em>Découvrir↗</em>
+          </a>
+        </p>
+      </aside>
+      <aside>
+        <img
+          css="width: 10rem"
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5f/Flag_of_Quebec.svg/640px-Flag_of_Quebec.svg.png"
+        ></img>
+        <h3> FabMob Québec 🏴</h3>
+        <p>Blablabla </p>
+        <p>
+          <a href="#">
+            <em>Découvrir↗</em>
+          </a>
+        </p>
+      </aside>
+    </section>
+  </main>
 );
