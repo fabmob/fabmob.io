91626bf94e2ac28450322c171f94a5a3295e80ce
Mael <mael.thomas@data.gouv.fr>
2020-08-11 15:58:38 +0200

    Le menu sur mobile s'active au clic

--- Nav.js
+++ Nav.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Emoji from './Emoji'
 import { LangSwitch, Link } from './Lang'
 import colors from './colors'
@@ -7,6 +7,7 @@ import { useLocation } from 'react-router-dom'
 
 export default () => {
 	let location = useLocation()
+	const [open, setOpen] = useState(false)
 	return (
 		<nav
 			css={`
@@ -51,11 +52,25 @@ export default () => {
 					width: 4rem;
 					${location.pathname === '/' ? 'display: none;' : ''}
 				}
+
+				@media (max-width: 800px) {
+					justify-content: space-evenly;
+
+					img {
+						width: 3rem;
+					}
+					ul {
+						flex-direction: column;
+						align-item: end;
+						${open ? 'display: flex' : 'display: none'}
+					}
+				}
 			`}
 		>
 			<Link to="/">
 				<img css="" src={LogoSansTexte} />
 			</Link>
+
 			<ul>
 				<li css="position: relative">
 					<Link to="/à-propos">Philosophie</Link>
@@ -88,6 +103,25 @@ export default () => {
 					<LangSwitch />
 				</li>
 			</ul>
+			<button
+				css={`
+					border: none;
+					padding: 0;
+					margin: 0;
+					svg {
+						width: 2rem;
+						height: 2rem;
+					}
+
+					display: none;
+					@media (max-width: 800px) {
+						display: block;
+					}
+				`}
+				onClick={() => setOpen(!open)}
+			>
+				{open ? <CloseMenuIcon /> : <MenuIcon />}
+			</button>
 		</nav>
 	)
 }
@@ -107,4 +141,24 @@ const Notifications = ({ count }) => (
 	>
 		{count}
 	</span>
+)
+
+const MenuIcon = () => (
+	<svg viewBox="0 0 100 80" width="40" height="40">
+		<rect width="100" height="20"></rect>
+		<rect y="30" width="100" height="20"></rect>
+		<rect y="60" width="100" height="20"></rect>
+	</svg>
+)
+
+const CloseMenuIcon = () => (
+	<svg
+		enable-background="new 0 0 386.667 386.667"
+		height="512"
+		viewBox="0 0 386.667 386.667"
+		width="512"
+		xmlns="http://www.w3.org/2000/svg"
+	>
+		<path d="m386.667 45.564-45.564-45.564-147.77 147.769-147.769-147.769-45.564 45.564 147.769 147.769-147.769 147.77 45.564 45.564 147.769-147.769 147.769 147.769 45.564-45.564-147.768-147.77z" />
+	</svg>
 )
