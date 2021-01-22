ce2fd10f7cfe65ea257f7aa83773e5f88204fdd1
Mael <mael.thomas@data.gouv.fr>
2021-01-22 10:09:12 +0100

    Réagencement de /ecosystem

--- pages/Ecosystem.js
+++ pages/Ecosystem.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { Link } from 'Components/Lang'
 import colors from 'Components/colors'
-import members from '../members.yaml'
+import ecosystème from '../ecosystème.yaml'
 import Emoji from 'Components/Emoji'
 import Page from 'Components/Page'
 
@@ -14,59 +14,9 @@ export default () => {
 		>
 			<header>
 				<h1>L'ecosystème FabMob</h1>
-				<p>
-					Le modèle de la fabrique, initié en France, se répliquer dans d'autres
-					pays et domaines
-				</p>
-				<div
-					css={`
-						display: flex;
-						justify-content: center;
-						align-items: center;
-						> button {
-							padding: 0.3rem;
-							margin: 1rem;
-							font-weight: bold;
-							text-transform: uppercase;
-							background: ${colors.bleu};
-							border: none;
-						}
-						img {
-							border-radius: 0.3rem;
-							height: 5rem;
-							display: block;
-						}
-					`}
-				>
-					<button
-						css={
-							filter.géo === 'france'
-								? `background: ${colors.vert} !important`
-								: ''
-						}
-						onClick={() =>
-							setFilter({ géo: filter.géo === 'france' ? null : 'france' })
-						}
-					>
-						<img src="https://images.unsplash.com/photo-1499856871958-5b9627545d1a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=80"></img>
-						<span>France</span>
-					</button>
-					<button
-						css={
-							filter.géo === 'québec'
-								? `background: ${colors.vert} !important`
-								: ''
-						}
-						onClick={() =>
-							setFilter({ géo: filter.géo === 'québec' ? null : 'québec' })
-						}
-					>
-						<img src="https://images.unsplash.com/photo-1558489580-faa74691fdc5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60"></img>
-						<span>Québec</span>
-					</button>
-				</div>
 			</header>
 			<Présentation filter={filter} />
+			<AutresFabriques />
 		</Page>
 	)
 }
@@ -94,32 +44,12 @@ let Présentation = ({ filter }) => (
 			<header>
 				<h2>L'équipe</h2>
 			</header>
-			<div css="display: flex;">
-				{members
-					.filter((el) => !filter.géo || el.géo === filter.géo)
-					.map(({ nom, image, rôle }) => (
-						<aside>
-							<img src={image} title={nom} />
-							<h3>{nom}</h3>
-							<p>{rôle}</p>
-						</aside>
-					))}
-			</div>
+			<Members data={ecosystème["L'équipe"]} />
 			<header>
 				<h2>Le conseil d'administration</h2>
 			</header>
 
-			<div css="display: flex;">
-				{members
-					.filter((el) => !filter.géo || el.géo === filter.géo)
-					.map(({ nom, image, rôle }) => (
-						<aside>
-							<img src={image} title={nom} />
-							<h3>{nom}</h3>
-							<p>{rôle}</p>
-						</aside>
-					))}
-			</div>
+			<Members data={ecosystème["Le conseil d'administration"]} />
 
 			<header>
 				<h2>Les adhérents</h2>
@@ -131,4 +61,55 @@ let Présentation = ({ filter }) => (
 			<p>Nous allons ici afficher les noms et logos de tous les adhérents</p>
 		</section>
 	</main>
+)
+
+const Members = ({ data }) => (
+	<div css="display:flex; justify-content: center;">
+		{data.map(({ nom, image, rôle }) => (
+			<aside>
+				<img src={image} title={nom} />
+				<h3>{nom}</h3>
+				<p>{rôle}</p>
+			</aside>
+		))}
+	</div>
+)
+
+const AutresFabriques = () => (
+	<div>
+		<h2>Les autres fabriques</h2>
+		<p>
+			Le modèle de la fabrique, initié en France, se répliquer dans d'autres
+			pays et domaines
+		</p>
+		<div
+			css={`
+				display: flex;
+				justify-content: center;
+				align-items: center;
+				> button {
+					padding: 0.3rem;
+					margin: 1rem;
+					font-weight: bold;
+					text-transform: uppercase;
+					background: ${colors.bleu};
+					border: none;
+				}
+				img {
+					border-radius: 0.3rem;
+					height: 5rem;
+					display: block;
+				}
+			`}
+		>
+			<button>
+				<img src="https://images.unsplash.com/photo-1499856871958-5b9627545d1a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=80"></img>
+				<span>France</span>
+			</button>
+			<button>
+				<img src="https://images.unsplash.com/photo-1558489580-faa74691fdc5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60"></img>
+				<span>Québec</span>
+			</button>
+		</div>
+	</div>
 )
