aaa892b49831c6f62eb309fad461332622e5855f
Mael <mael.thomas@data.gouv.fr>
2021-02-05 14:29:48 +0100

    MAJ dépendances

--- webpack.config.js
+++ webpack.config.js
@@ -57,7 +57,15 @@ module.exports = {
 					{
 						loader: require.resolve('babel-loader'),
 						options: {
-							presets: ['@babel/preset-env', '@babel/preset-react'],
+							presets: [
+								'@babel/preset-env',
+								[
+									'@babel/preset-react',
+									{
+										runtime: 'automatic',
+									},
+								],
+							],
 							plugins: [
 								'babel-plugin-styled-components',
 								isDevelopment && require.resolve('react-refresh/babel'),
@@ -74,10 +82,10 @@ module.exports = {
 	},
 
 	output: {
-		filename: 'index.js',
-		path: __dirname + '/dist',
+		path: path.join(__dirname, 'dist'),
 	},
 	devServer: {
+		contentBase: path.join(__dirname, 'dist'),
 		historyApiFallback: true,
 	},
 
