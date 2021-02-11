bf45cc27ab082c8c7b5e6b35081cda26b1a6bb77
Mael <mael.thomas@data.gouv.fr>
2021-02-11 16:02:11 +0100

    Bon dossier output

--- code/webpack.config.js
+++ code/webpack.config.js
@@ -83,7 +83,7 @@ module.exports = {
 
 	output: {
 		filename: 'index.js',
-		path: path.join(__dirname, 'dist'),
+		path: path.join(__dirname, '../dist'),
 	},
 	devServer: {
 		contentBase: path.join(__dirname, 'dist'),
