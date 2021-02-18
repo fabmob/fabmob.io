272cd9b99f1c0118e72d3be181c9dbe37c60e47e
Mael <mael.thomas@data.gouv.fr>
2021-02-18 16:41:35 +0100

    Récupération des images de couverture des anciens articles
    Merci Wordpress pour ce travail ingrat

--- code/scripts/downloadImages.js
+++ code/scripts/downloadImages.js
@@ -1,5 +1,25 @@
-const fs = require('fs')
+const fs = require('fs').promises
+const fetch = require('node-fetch')
 
-fs.readFile('./articleImages.json', 'utf8', (error, data) => {
-	JSON.parse(data).map(({ url, coverImage }) => console.log(url))
-})
+async function download() {
+	const json = await fs.readFile('./code/scripts/articleImages.json', 'utf8'),
+		data = JSON.parse(json)
+
+	data.map(async ({ url, coverImage }) => {
+		const id = url.split('/').slice(-2)[0]
+		const extension = coverImage.split('.').slice(-1)[0]
+
+		if (!coverImage.includes('http')) return null
+		//		console.log(url, coverImage, encodeURI(coverImage), 'BLBLBLB')
+		const response = await fetch(encodeURI(coverImage)).catch((err) =>
+			console.log('OUPS', coverImage)
+		)
+		const buffer = await response.buffer()
+		fs.writeFile(
+			`contenu/articles/couvertures/${id}.${extension}`,
+			buffer,
+			() => console.log('OK ' + url)
+		)
+	})
+}
+download()
