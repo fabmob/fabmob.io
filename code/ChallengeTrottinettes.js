6123b7f69daa0a9346b82c7b2ba4d9a141e56e03
Mael <mael.thomas@data.gouv.fr>
2021-03-04 11:57:51 +0100

    Page challenge trottinettes

---
+++ code/ChallengeTrottinettes.js
@@ -0,0 +1,19 @@
+export default () => (
+	<div css="max-width: 800px; margin: 0 auto">
+		<h1>Challenge national des trottinettes</h1>
+		<p>Rejoignez-nous ! </p>
+		<div css="display: flex; justify-content: space-evenly">
+			<a href="https://forum.fabmob.io/c/nos-communs/2nde-vie-des-trottinettes/32">
+				<button>Participez sur le forum</button>
+			</a>
+			<a href="https://wiki.lafabriquedesmobilites.fr/wiki/Accueil">
+				<button>Découvrez les communs</button>
+			</a>
+		</div>
+		<img src="https://forum.fabmob.io/uploads/default/original/1X/a9144b5024349ce536194e005cbb889db9c116c7.jpeg" />
+		<a href="#infos">
+			<button>Plus d'infos ⬇️</button>
+		</a>
+		<div id="#infos">blabla</div>
+	</div>
+)
