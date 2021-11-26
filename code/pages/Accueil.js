0c76367b8e8efcdaefaf9d28fa61425c6ba3fd7a
GabrielPlassat <transportsdufutur@gmail.com>
2021-11-26 22:00:31 +0100

    Update Accueil.js
    bouton rejoignez nous

--- code/pages/Accueil.js
+++ code/pages/Accueil.js
@@ -86,6 +86,12 @@ export default () => {
 							to="https://www.helloasso.com/associations/la-fabrique-des-mobilites/adhesions/adhesions-2021-2022"
 							img={<CTAArrow />}
 						/>
+								
+						<CTA
+							text="Rejoignez nous"
+							to="mailto:contact@fabmob.io?subject=Poste"
+							img={<CTAArrow />}
+						/>
 					</div>
 					<br />
 				</section>
